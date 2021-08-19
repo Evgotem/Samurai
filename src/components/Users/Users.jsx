e008ff509dea2109e24592bc16918f4eca8a0a8f
Evgotem <57010853+Evgotem@users.noreply.github.com>
2021-08-19 17:00:17 +0400

    Подключена библиотека axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,30 +7,20 @@ const Users = (props) => {
 
    if (props.users.length === 0) {
       props.setUsers([
-         {
-            id: 1, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Evgeny', status: 'I am learning React right now', location: { city: 'Samara', country: 'Russia' }
-         },
-         {
-            id: 2, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Kristy', status: 'I am write an article today', location: { city: 'Samara', country: 'Russia' }
-         },
-         {
-            id: 3, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Oleg', status: 'I bought an apartment', location: { city: 'Samara', country: 'Russia' }
-         },
-         {
-            id: 4, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: false, fullName: 'Sasha', status: 'I stand and guard the store', location: { city: 'Samara', country: 'Russia' }
-         },
-         {
-            id: 5, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Ruzil', status: 'I am just student', location: { city: 'Novo-Usmanovo', country: 'Russia' }
-         },
-         {
-            id: 6, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Guzel', status: 'I listening to music', location: { city: 'Novo-Usmanovo', country: 'Russia' }
-         },
-         {
-            id: 7, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: false, fullName: 'Alex', status: 'Swimming in the smoke', location: { city: 'Pestravka', country: 'Russia' }
-         },
-         {
-            id: 8, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Regulus', status: 'It was stolen', location: { city: 'Hogwards', country: 'UK' }
-         },
+         { id: 1, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Evgeny', status: 'I am learning React right now', location: { city: 'Samara', country: 'Russia' } 
+      },
+         { id: 2, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Kristy', status: 'I am write an article today', location: { city: 'Samara', country: 'Russia' }
+       },
+         { id: 3, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Oleg', status: 'I bought an apartment', location: { city: 'Samara', country: 'Russia' } 
+      },
+         { id: 4, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: false, fullName: 'Sasha', status: 'I stand and guard the store', location: { city: 'Samara', country: 'Russia' } 
+      },
+         { id: 5, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Ruzil', status: 'I am just student', location: { city: 'Novo-Usmanovo', country: 'Russia' } 
+      },
+         { id: 6, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Guzel', status: 'I listening to music', location: { city: 'Novo-Usmanovo', country: 'Russia' } 
+      },
+         { id: 7, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: false, fullName: 'Alex', status: 'Swimming in the smoke', location: { city: 'Pestravka', country: 'Russia' } },
+         { id: 8, photoUrl: 'https://zooclub.ru/attach/8420.jpg', followed: true, fullName: 'Regulus', status: 'It was stolen', location: { city: 'Hogwards', country: 'UK' } },
       ]);
    }
 
