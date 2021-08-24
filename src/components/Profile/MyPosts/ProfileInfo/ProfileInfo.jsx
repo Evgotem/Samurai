d8db6a5236a3c1710d4c06ee1cbd34a9a2b05266
Evgotem <57010853+Evgotem@users.noreply.github.com>
2021-08-24 15:55:56 +0400

    Подключен WithRouter для взаимодействия с URL и отображение юзеров в профиле при клике на них

--- src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileInfo.jsx
@@ -4,14 +4,26 @@ import style from './ProfileInfo.module.scss';
 
 const ProfileInfo = (props) => {
 
-   if(!props.profile) {
+   if (!props.profile) {
       return <Preloader />
    }
-debugger;
 
    return (
       <div>
-         <img src={props.profile.photos.large} alt="Avatar" />
+         <div>
+            <img src={props.profile.photos.large} alt="Avatar" />
+         </div>
+         <div>
+            My name is: {props.profile.fullName}
+         </div>
+         <div className={style.about}>
+            {props.profile.aboutMe}
+         </div>
+         <div>
+            <p>Looking for a job status: {props.profile.lookingForAJob ? 'true' : 'false'}</p>
+            <p>Description: {props.profile.lookingForAJobDescription}</p>
+         </div>
+
       </div>
    )
 }
